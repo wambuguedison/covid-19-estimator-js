f844bb0f303435aceb74ace773ad0d468b651ff4
Eddie Waweru <wambuguedison@gmail.com>
2020-04-08 09:59:54 +0300

    Return statement

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,4 @@
 const output = {
-<<<<<<< HEAD
   data: {},
   impact: {},
   severeImpact: {}
@@ -20,28 +19,8 @@ const covid19ImpactEstimator = (data) => {
 
   output.impact.infectionsByRequestedTime = infectionsAtaTime(output.impact.currentlyInfected, data.timeToElapse);
   output.severeImpact.infectionsByRequestedTime = infectionsAtaTime(output.severeImpact.currentlyInfected, data.timeToElapse);
-=======
-    data: {},
-    impact: {},
-    severeImpact: {}
-}
-
-const infectionsAtaTime = (currentlyInfected, days) => {
-    let setsOfDays = (days / 3).toFixed();
-    setsOfDays = Number(setsOfDays);
-    return currentlyInfected * (Math.pow(2, setsOfDays));
-}
-
-const covid19ImpactEstimator = (data) => {
-    let currentlyInfected = data['reportedCases'] * 10;
-    output.impact.currentlyInfected = currentlyInfected;
-    
-    let severelyInfected = data['reportedCases'] * 50;
-    output.severeImpact.currentlyInfected = severelyInfected;
-
-    output.impact.infectionsByRequestedTime = infectionsAtaTime(output.impact.currentlyInfected, data.timeToElapse);
-    output.severeImpact.infectionsByRequestedTime = infectionsAtaTime(output.severeImpact.currentlyInfected, data.timeToElapse);
->>>>>>> d26ac88636054eb5f7b6a7a232f119fe0fb2d649
+  
+  return data;
 };
 
 export default covid19ImpactEstimator;
