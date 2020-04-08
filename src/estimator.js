3ac805a422131fb2d13636df96856f8ef9924fcd
Eddie Waweru <wambuguedison@gmail.com>
2020-04-08 09:43:42 +0300

    challenge 1 done

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,24 @@
-const covid19ImpactEstimator = (data) => data;
+const output = {
+  data: {},
+  impact: {},
+  severeImpact: {}
+};
+
+const infectionsAtaTime = (currentlyInfected, days) => {
+  let setsOfDays = (days / 3).toFixed();
+  setsOfDays = Number(setsOfDays);
+  return currentlyInfected * (2 ** setsOfDays);
+};
+
+const covid19ImpactEstimator = (data) => {
+  let currentlyInfected = data['reportedCases'] * 10;
+  output.impact.currentlyInfected = currentlyInfected;
+    
+  let severelyInfected = data['reportedCases'] * 50;
+  output.severeImpact.currentlyInfected = severelyInfected;
+
+  output.impact.infectionsByRequestedTime = infectionsAtaTime(output.impact.currentlyInfected, data.timeToElapse);
+  output.severeImpact.infectionsByRequestedTime = infectionsAtaTime(output.severeImpact.currentlyInfected, data.timeToElapse);
+};
 
 export default covid19ImpactEstimator;
